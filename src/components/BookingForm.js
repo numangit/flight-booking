c2212e2005d737466632078b94f5a9ae4d8e0c58
numangit <abdulnumanjob@gmail.com>
2023-02-24 16:57:51 +0600

    warnings cleared

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -94,8 +94,8 @@ const BookingForm = () => {
                     </div>
 
                     <button className="addCity" type="submit" id="lws-addCity">
-                        <svg width="15px" height="15px" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        <svg width="15px" height="15px" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
+                            <path strokeLinecap="round" strokeinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
                         <span className="text-sm">Book</span>
                     </button>
