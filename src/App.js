e7e7417c0535e3103ec4cf73eba8834e9844dfaa
numangit <abdulnumanjob@gmail.com>
2023-02-24 11:43:28 +0600

    preview component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BookingForm from './components/BookingForm';
 import Header from './components/Header';
+import PreviewSection from './components/PreviewSection';
 
 function App() {
   return (
     <div>
       <Header />
       <BookingForm />
+      <PreviewSection />
     </div>
   );
 }
